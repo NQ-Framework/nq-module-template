5a905c8adb74758ffd8f60203afa14374a3886e9
Milos Spasojevic <milos.s.pfc@gmail.com>
2020-10-26 22:02:52 +0100

    Add package.json replacements

--- initialize/initialize.js
+++ initialize/initialize.js
@@ -3,8 +3,24 @@ const chalk = require('chalk');
 const log = console.log;
 
 log(chalk`{green Welcome to the }{green.bold NQ Framework}{green  module setup!}`);
-const name = ps(chalk`{green Your project name: }`);
-console.log("you entered " + name);
 
 const fs = require('fs');
-fs.rmdirSync('../initialize', { recursive: true });
+const folderNames = process.cwd().split('/');
+const defaultName = folderNames && folderNames.length >= 2 ? folderNames[folderNames.length - 2] : 'nq module';
+console.log(defaultName);
+
+let inputName = ps(chalk`{green Your project name: (${defaultName}) }`);
+const name = inputName || defaultName;
+
+const defaultDescription = "An NQ Framework module project";
+let shortDescription = ps(chalk`{green Short description: }`);
+const description = shortDescription || defaultDescription;
+
+let pjson = fs.readFileSync('../package.json', 'utf8');
+pjson = pjson.replace('"name": "nq-module-template"', `"name": "${name}"`)
+pjson = pjson.replace('This is a template repository for adding new NQ framework modules', description);
+pjson = pjson.replace('git+https://github.com/nq-framework/nq-local-connector.git', `git+https://github.com/nq-framework/${name}.git`)
+fs.writeFileSync('../package.json', pjson, 'utf8');
+
+
+// fs.rmdirSync('../initialize', { recursive: true });
